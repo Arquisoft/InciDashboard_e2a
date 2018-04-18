394bcf07ce4b1edef130a88ec9ee471180694131
pobefer <36231197+pobefer@users.noreply.github.com>
2018-04-18 18:14:16 +0200

    travis reload

--- src/test/java/asw/test/steps/OperatorListarIncidenciasSteps.java
+++ src/test/java/asw/test/steps/OperatorListarIncidenciasSteps.java
@@ -53,11 +53,11 @@ public class OperatorListarIncidenciasSteps {
   
   @Given("^(\\d+) incidences$")
   public void incidences(int incidences) throws Throwable {
-	 oService.addOperator(new Operator("NonAddedOperator1", "pepe2"));
+	 oService.addOperator(new Operator("NonAddedOperator2", "pepe2"));
     for(int i = 0 ; i< incidences ;i++){
     	Incidence in = new Incidence("i"+i,null,null,null);
     	iService.addIncidence(in );
-    	o = oService.getOperatorByName("NonAddedOperator1") ;
+    	o = oService.getOperatorByName("NonAddedOperator2") ;
     	o.addIncidencia(in) ;
     	in.setOperadorAsignado(o);
     	
